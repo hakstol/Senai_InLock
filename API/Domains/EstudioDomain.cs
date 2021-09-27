e1a9bc36df57e40cebb3e91d9741186d9cb50594
Leonardo Vitor Ferreira da SIlva <61870020+lyogaki@users.noreply.github.com>
2021-09-27 16:44:09 -0300

    Create EstudioDomain.cs

---
+++ API/Domains/EstudioDomain.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi_.Domains
+{
+    public class EstudioDomain
+    {
+        public int idEstudio { get; set; }
+        public string nomeEstudio { get; set; }
+    }
+}
